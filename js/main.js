8b9d1badf27f3aed692decf1e4241faefc45e40a
CaosMen <bruno.lemos.ec@gmail.com>
2020-05-21 10:06:18 -0300

    Error message added.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,35 @@
-function Convert (event) {
-    event.preventDefault();
-
-    var BinaryInput = document.getElementById("BinaryInput").value;
-    var DecimalOutput = 0;
-
-    if (BinaryInput != "") {
-        for(i = 0; BinaryInput != 0; i++){
-            DecimalOutput += Math.pow(2, i) * parseInt(BinaryInput[BinaryInput.length - 1]);
-            BinaryInput = BinaryInput.substring(0, BinaryInput.length - 1);
+function Convert () {
+    var InputBinary = document.getElementById("BinaryInput"); 
+    if (!InputBinary.checkValidity()) { 
+        if(InputBinary.validity.patternMismatch){
+            document.getElementById("validation").innerHTML = "PLEASE ENTER ONLY ONE'S AND ZERO'S!";
         }
+    } else { 
+        document.getElementById("validation").innerHTML = ""; 
+
+        var BinaryInput = document.getElementById("BinaryInput").value;
+        var DecimalOutput = 0;
     
-        document.getElementById("DecimalOutput").value = DecimalOutput;
-    } else {
-        document.getElementById("DecimalOutput").value = "";
-    }
+        if (BinaryInput != "") {
+            for(i = 0; BinaryInput != 0; i++){
+                DecimalOutput += Math.pow(2, i) * parseInt(BinaryInput[BinaryInput.length - 1]);
+                BinaryInput = BinaryInput.substring(0, BinaryInput.length - 1);
+            }
+        
+            document.getElementById("DecimalOutput").value = DecimalOutput;
+        } else {
+            document.getElementById("validation").innerHTML = "PLEASE ENTER A BINARY NUMBER!";
+            
+            document.getElementById("DecimalOutput").value = "";
+        }
+    } 
 }
 
 function ResetAll () {
     document.getElementById("BinaryInput").value = "";
     document.getElementById("DecimalOutput").value = "";
+
+    document.getElementById("validation").innerHTML = ""; 
 }
 
 function CopyInfo () {
